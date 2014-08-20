b60dcf020a35c6a99e6ae3fc25a6f0cc819cf6e0
natalialany <natalia.lany@makingwaves.pl>
2014-08-20 20:54:43 +0200

    htmlBoard-draw-NL: prototype code

--- classes/HTMLBoard.class.js
+++ classes/HTMLBoard.class.js
@@ -12,6 +12,38 @@
     }
 
     this.draw = function () {
+      var container = options.container;
+      var board = options.board;
+
+      var tileClasses = {
+        'X': 'tile--wall',
+        '#': 'tile--ground',
+        'O': 'tile--rock',
+        'E': 'tile--start',
+        '*': 'tile--catch',
+        'S': 'tile--end'
+      }
+
+      var boardNode = document.createElement('div');
+      boardNode.setAttribute('class', 'board-wrapper');
+
+      for (var i = 0, h = board.getHeight(); i < h; i++) {
+        for (var j = 0, w = board.getWidth(); j < w; j++) {
+
+          var tileNode = document.createElement('div');
+          tileNode.setAttribute('class', 'tile ' + tileClasses[board.getTile(j, i)._type]);
+          tileNode.style.width = (100 / board.getWidth()) + "%";
+
+          var tileInnerNode = document.createElement('div');
+          tileInnerNode.setAttribute('class', 'tile__inner');
+
+          tileNode.appendChild(tileInnerNode);
+          boardNode.appendChild(tileNode);
+        }
+      }
+
+      container.appendChild(boardNode);
+
 
     };
 
