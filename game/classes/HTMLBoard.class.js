f09068e373a710702fad6ae6780ae2e4af97c16c
natalialany <vviedzminka@gmail.com>
2014-09-01 17:38:08 +0200

    htmlBoard-anim-NL: refactoring

--- game/classes/HTMLBoard.class.js
+++ game/classes/HTMLBoard.class.js
@@ -1,14 +1,13 @@
 (function () {
   window.HTMLBoard = function (options) {
-    var that = this, game = options.game;
-    var moleClass = 'down';
+    var that = this, game = options.game, moleClass = 'down';
 
     function onKeyDown(e) {
         switch(e.keyCode) {
-          case 38: game.nextMoleMove('up'); break;
-          case 40: game.nextMoleMove('down'); break;
-          case 37: game.nextMoleMove('left'); break;
-          case 39: game.nextMoleMove('right'); break;
+          case 38: game.nextMoleMove('up'); changeMoleLook('up'); break;
+          case 40: game.nextMoleMove('down'); changeMoleLook('down'); break;
+          case 37: game.nextMoleMove('left'); changeMoleLook('left'); break;
+          case 39: game.nextMoleMove('right'); changeMoleLook('right'); break;
         }
     }
 
@@ -28,40 +27,30 @@
 
         console.log('object ', data.id, ' moved from X:', data.from.x, ', Y:', data.from.y, '  to X:', data.to.x, ', Y:', data.to.y);
 
+        //Moving objects animation
         var el = document.querySelector("#item_" + data.id);
         el.classList.add('anim');
 
         //transformations for moving
         if (data.from.x > data.to.x) {
           el.classList.add('anim--left');
-          if (data.id===20) { moleClass = 'left'; }
         }
         if (data.from.x < data.to.x) {
           el.classList.add('anim--right');
-          if (data.id===20) { moleClass = 'right'; }
         }
         if (data.from.y > data.to.y) {
           el.classList.add('anim--up');
-          if (data.id===20) { moleClass = 'up'; }
         }
         if (data.from.y < data.to.y) {
           el.classList.add('anim--down');
-          if (data.id===20) { moleClass = 'down'; }
         }
-
-        var mole = document.querySelector(".tile--mole");
-        mole.classList.add(moleClass);
-
 
         /* Listen for a transition! */
         /* Solution taken from http://davidwalsh.name/css-animation-callback */
         var transitionEvent = whichTransitionEvent(el);
         transitionEvent && el.addEventListener(transitionEvent, function() {
           console.log('Transition complete!  This is the callback, no library needed!');
-
           that.draw();
-          var mole = document.querySelector(".tile--mole");
-          mole.classList.add(moleClass);
           drawFinished = true;
         });
 
@@ -105,6 +94,9 @@
           var tile = board.getTile(j,i);
           var newNode = tileNode.cloneNode(true);
           newNode.classList.add('tile--' + tile.getType());
+          if (tile.getType()==="mole") {
+            newNode.classList.add(moleClass);
+          }
           newNode.id = 'item_' + tile.getId();
           boardNode.appendChild(newNode);
         }
@@ -112,6 +104,16 @@
 
       DOMHelper.purgeElement(container);
       container.appendChild(boardNode);
+    };
+
+    var changeMoleLook  = function(direction) {
+      moleClass = direction;
+      var mole = document.querySelector(".tile--mole");
+      mole.classList.remove('up');
+      mole.classList.remove('down');
+      mole.classList.remove('left');
+      mole.classList.remove('right');
+      mole.classList.add(moleClass);
     };
 
     this.destroy = function () {
