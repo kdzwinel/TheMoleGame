213ef897cfd325bbad88981991d0587de6b55da6
Konrad Dzwinel <kdzwinel@gmail.com>
2017-02-21 00:14:36 +0100

    Update favicon and sw

--- serviceworker.js
+++ serviceworker.js
@@ -1,7 +1,16 @@
 const resources = [
   '/',
   'game.js',
-  'main.css'
+  'main.css',
+  'manifest.json',
+  'favicon.ico',
+  'icons/icon-72x72.png',
+  'icons/icon-96x96.png',
+  'icons/icon-128x128.png',
+  'icons/icon-144x144.png',
+  'icons/icon-152x152.png',
+  'icons/icon-192x192.png',
+  'icons/icon-384x384.png'
 ];
 
 this.addEventListener('install', event => {
